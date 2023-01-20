9af85190fbb1df4b571e23b248f8cbaca7a4d8ca
Ben Arno <bdarno92@gmail.com>
2023-01-20 01:49:52 +0000

    finished script

---
+++ PasswordAndSMB.ps1
@@ -0,0 +1,12 @@
+#!/usr/bin/env powershell
+
+# Enable password complexity
+# Courtesy of this dude: https://social.technet.microsoft.com/Forums/office/en-US/50c827f8-0b8f-4b07-a90f-582948187ccb/disabling-password-complexity-via-powershell?forum=winserverpowershell
+# I copied, pasted, and switched the 0 and the 1, because binary.
+secedit /export /cfg c:\secpol.cfg
+(gc C:\secpol.cfg).replace("PasswordComplexity = 1", "PasswordComplexity = 0") | Out-File C:\secpol.cfg
+secedit /configure /db c:\windows\security\local.sdb /cfg c:\secpol.cfg /areas SECURITYPOLICY
+rm -force c:\secpol.cfg -confirm:$false
+
+# SMB Server Config script from Zac, proving it's disabled:
+Get-SmbServerConfiguration | Format-List EnableSMB1Protocol
