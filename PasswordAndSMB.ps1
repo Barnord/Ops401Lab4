3e9e923102319d89bfa995caed430b2273967a93
Ben Arno <bdarno92@gmail.com>
2023-01-20 02:03:56 +0000

    binary, amirite?

--- PasswordAndSMB.ps1
+++ PasswordAndSMB.ps1
@@ -4,7 +4,7 @@
 # Courtesy of this dude: https://social.technet.microsoft.com/Forums/office/en-US/50c827f8-0b8f-4b07-a90f-582948187ccb/disabling-password-complexity-via-powershell?forum=winserverpowershell
 # I copied, pasted, and switched the 0 and the 1, because binary.
 secedit /export /cfg c:\secpol.cfg
-(gc C:\secpol.cfg).replace("PasswordComplexity = 1", "PasswordComplexity = 0") | Out-File C:\secpol.cfg
+(gc C:\secpol.cfg).replace("PasswordComplexity = 0", "PasswordComplexity = 1") | Out-File C:\secpol.cfg
 secedit /configure /db c:\windows\security\local.sdb /cfg c:\secpol.cfg /areas SECURITYPOLICY
 rm -force c:\secpol.cfg -confirm:$false
 
